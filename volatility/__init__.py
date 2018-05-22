af4907283dd7bd73f25769b4d857af8f7ec252e0
4n6ist <forensics@kazamiya.net>
2018-05-23 00:06:37 +0900

    add: working community plugins

--- volatility/__init__.py
+++ volatility/__init__.py
@@ -2,6 +2,6 @@ import logging
 import lib2to3.pgen2.driver
 class Lib2to3LoggingModuleShim(object):
     def getLogger(self):
-	    return logging.getLogger('lib2to3')
+        return logging.getLogger('lib2to3')
 lib2to3.pgen2.driver.logging = Lib2to3LoggingModuleShim()
 logging.getLogger('lib2to3').setLevel(logging.ERROR)
